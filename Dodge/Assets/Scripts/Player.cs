4f3d00b001606bcec94263b93cfee68a11b59327
William Ngo <ngo.willi@gmail.com>
2017-12-16 21:36:00 -0500

    Character gravity and movement
    Added gravity and character moving left right. Gravity is acting weird, gonna tweak that.

---
+++ Dodge/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RaycastController))]
+[RequireComponent(typeof(PlayerInput))]
+public class Player : MonoBehaviour {
+
+	public float gravity; //Amount of velocity y to fall
+	public float jumpHeight; //Height of jump
+	public float speed; //Horizontal movement speed
+	public int facing = 1; //1 means facing right, -1 means left
+
+	public Vector2 velocity;
+	public Vector2 directionalInput;
+
+	public RaycastController rc;
+
+	// Use this for initialization
+	void Start () {
+		rc = GetComponent<RaycastController> ();
+
+		gravity = -(2 * 2.5f) / Mathf.Pow(0.32f, 2);
+		speed = 7f;
+		jumpHeight = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		getPlayerInput ();
+		velocity.x = directionalInput.x * speed;
+		velocity.y += gravity * Time.deltaTime;
+		Move (velocity * Time.deltaTime);
+
+		if (rc.collision.below) {
+			velocity.y = 0f;
+		}
+	}
+
+	public void Move(Vector2 moveAmount)
+	{
+		rc.UpdateRayOrigins ();
+		rc.collision.Reset ();
+
+		//facing
+		facing = (int)Mathf.Sign(moveAmount.x);
+
+		//Check collisions - if found, moveAmount velocity will be reduced appropriately
+		rc.checkCollisions (ref moveAmount);
+
+		//Actually changing the velocity of game object
+		transform.Translate (moveAmount);
+	}
+
+	private void getPlayerInput()
+	{
+		this.directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+	}
+}
---
+++ Dodge/Assets/Scripts/RaycastController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RaycastController : MonoBehaviour {
+
+	public LayerMask collisionMask; //Layer in which to detect a collision
+
+	public const float widthBuffer = 0.015f; //Buffer distance
+	private const float dstBetweenRays = 0.25f; //Should have 4 rays for each side
+
+	public int rayCount_h; //number of horizontal rays
+	public int rayCount_v; //number of vertical rays
+
+	public float raySpacing_h;
+	public float raySpacing_v;
+
+	public BoxCollider2D box; //box collider of game object
+
+	//Raycast origins
+	public Vector2 topLeft;
+	public Vector2 topRight;
+	public Vector2 botLeft;
+	public Vector2 botRight;
+
+	//Collisions information
+	public CollisionInformation collision;
+
+	public virtual void Awake(){
+		box = GetComponent<BoxCollider2D> ();
+	}
+	void Start () {
+		/*** Set up raycasts ***/
+		//Get bounds of boxcollider with a bit of buffer
+		Bounds bounds = box.bounds;
+		bounds.Expand (widthBuffer * -2);
+
+		float width = bounds.size.x;
+		float height = bounds.size.y;
+
+		//Calculate number of rays
+		rayCount_h = Mathf.RoundToInt (height / dstBetweenRays);
+		rayCount_v = Mathf.RoundToInt (width / dstBetweenRays);
+
+		//Space them out evenly
+		raySpacing_h = height / (rayCount_h - 1);
+		raySpacing_v = width / (rayCount_v - 1);
+	}
+
+	/**
+	 * Update point of origin of raycast when boxcollider moves.
+	 *
+	 */
+	public void UpdateRayOrigins()
+	{
+		//Get bounds of boxcollider with a bit of buffer
+		Bounds bounds = box.bounds;
+		bounds.Expand (widthBuffer * -2);
+
+		//set raycast origin point
+		topLeft = new Vector2 (bounds.min.x, bounds.max.y);
+		topRight = new Vector2 (bounds.max.x, bounds.max.y);
+		botLeft = new Vector2 (bounds.min.x, bounds.min.y);
+		botRight = new Vector2 (bounds.max.x, bounds.min.y);
+	}
+
+	public void checkCollisions(ref Vector2 moveAmount){
+		
+		if (moveAmount.y != 0) {
+			checkVerticalCollisions (ref moveAmount);
+		}
+	}
+
+	private void checkHorizontalCollision(ref Vector2 moveAmount)
+	{
+
+	}
+
+	private void checkVerticalCollisions(ref Vector2 moveAmount)
+	{
+		float directionY = Mathf.Sign (moveAmount.y);
+		float rayLength = Mathf.Abs (moveAmount.y) + widthBuffer;
+
+		//firing raycast
+		for (int i = 0; i < rayCount_v; i++) {
+			Vector2 rayOrigin = (directionY == -1) ? botLeft : topLeft;
+			rayOrigin += Vector2.right * (raySpacing_v * i + moveAmount.x);
+			RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+			Debug.DrawRay (rayOrigin, Vector2.up * directionY, Color.red);
+
+			//If collision found, update moveAmount.y by reducing the change in velocity
+			if (hit) {
+				moveAmount.y = (hit.distance - widthBuffer) * directionY;
+				rayLength = hit.distance;
+			}
+
+			//Set collision bool
+			collision.below = (directionY == -1);
+			collision.above = (directionY == 1);
+		}
+	}
+
+	public struct CollisionInformation
+	{
+		public bool above, below;
+		public bool left, right;
+
+		public bool climbingSlope;
+		public bool descendingSlope;
+		public float slopeAngle, slopeAngleOld;
+		public Vector2 moveAmountOld;
+		public int faceDir;
+		public bool fallingThroughPlatform;
+		public bool standingOnPlatform;
+
+		public void Reset()
+		{
+			above = below = false;
+			left = right = false;
+			climbingSlope = false;
+			descendingSlope = false;
+
+			slopeAngleOld = slopeAngle;
+			slopeAngle = 0f;
+		}
+	}
+}
