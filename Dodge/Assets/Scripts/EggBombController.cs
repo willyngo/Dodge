e46ffb3e2454559a3789b1c6ea506e43439cb706
Shifat Khan <shifatkhan6@gmail.com>
2017-12-17 21:39:29 -0500

    Created an Enemy Bird
    Bird drop bombs that explodes on impact with the ground. Need to add a "prefab destroyer" out of bounds to destroy the bird once it gets out of the screen.

---
+++ Dodge/Assets/Scripts/EggBombController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggBombController : MonoBehaviour {
+
+	public Animator anim;
+
+	public Rigidbody2D rigidBody;
+
+	void Start() {
+		rigidBody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag.Equals("Ground")) {
+			rigidBody.isKinematic = true;
+			rigidBody.velocity = Vector2.zero;
+			anim.Play ("Explosion");
+			Destroy(gameObject, 1); // Destroyed after 1 second
+		}
+	}
+
+}
---
+++ Dodge/Assets/Scripts/LinearMovementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearMovementController : MonoBehaviour {
+
+	public float speed = 10;
+
+	public string direction = "right";
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(direction.Equals("left", System.StringComparison.InvariantCultureIgnoreCase)){
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		else if(direction.Equals("right", System.StringComparison.InvariantCultureIgnoreCase)){
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		else if(direction.Equals("up", System.StringComparison.InvariantCultureIgnoreCase)){
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		else if(direction.Equals("down", System.StringComparison.InvariantCultureIgnoreCase)){
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+	}
+}
