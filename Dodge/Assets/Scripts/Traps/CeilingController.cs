4e0cb39f2cbaa4f05e034c0f18d71ef4d0b20aa1
ShiftKun <shifatkhan6@gmail.com>
2017-12-22 02:04:40 -0500

    Added 1 ceiling squash
    Player's x and y coordinates are checked in the ceiling's script to see if the player is squashed or not.

---
+++ Dodge/Assets/Scripts/Traps/CeilingController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingController : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y <= -4.04 
+			&& player.transform.position.x >= -4.7 && player.transform.position.y <= -3.079) {
+			Debug.Log ("SQUASHED");
+		}
+	}
+}
