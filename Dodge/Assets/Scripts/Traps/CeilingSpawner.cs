4d135910c77d03fa20e37e28d25bb9194cea44d5
Shifat Khan <shifatkhan6@gmail.com>
2017-12-24 16:44:01 -0500

    Added a ceiling spawner
    Added a prefab spawner that can spawn multiple prefabs instead of only one. Ceiling spawner is a variation of that.

---
+++ Dodge/Assets/Scripts/Traps/CeilingSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingSpawner : MonoBehaviour {
+
+	private float nextSpawn = 0;
+	public GameObject[] prefabsToSpawn;
+	public float spawnRate = 1;
+	public float randomDelay = 1;
+	private int index = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > nextSpawn) {
+			index = Random.Range (0, prefabsToSpawn.Length);
+			Instantiate (prefabsToSpawn[index], prefabsToSpawn[index].transform.position, prefabsToSpawn[index].transform.rotation);
+			nextSpawn = Time.time + spawnRate + Random.Range(0, randomDelay);
+		}
+	}
+}
