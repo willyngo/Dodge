00884be9376c660a972403d0a7782579dc28b8fe
Shifat Khan <shifatkhan6@gmail.com>
2017-12-30 18:11:46 -0500

    Added a game master and a general spawner

---
+++ Dodge/Assets/Scripts/Wave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave {
+
+	public string name;
+
+	// Reference to enemy prefab
+	public Transform enemy;
+
+	// Countdown to next wave
+	public int count;
+	public float rate;
+
+}
